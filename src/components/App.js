5f89dcbd1ce849e6853207e508e3f342a42524ca
DAVIDMWELU <davidmwelu@gmail.com>
2025-02-22 07:10:04 +0300

    simple data fetchint lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,31 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [image, setImage] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("https://dog.ceo/api/breeds/image/random");
+        const data = await response.json();
+        setImage(data.message);
+      } catch (error) {
+        console.error("Error fetching image:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Random Dog</h1>
+      {loading ? <p>Loading...</p> : <img src={image} alt="A Random Dog" />}
+    </div>
+  );
+}
+
+export default App;
